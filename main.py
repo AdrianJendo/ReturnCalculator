05d0aae3ab45e8764f6cc544d0249c2324fbc236
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-23 11:20:20 -0800

    Add gitignore

--- main.py
+++ main.py
@@ -12,4 +12,7 @@ if __name__ == "__main__":
     ticker = sys.argv[1]
     start_date = sys.argv[2]
     end_date = sys.argv[3]
+
+    # /SPY?serietype=line&apikey=455df261819241f4292b36bf952efbe9
+
     get_price_df(ticker, start_date, end_date)
