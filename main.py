2a484fbcc0e07ea84d448dd764bbd197821f8c51
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-23 12:57:31 -0800

    Add pandas and plot response as dataframe

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import sys
 import requests
+import os
+import json
+import pandas as pd
+
+API_URL = os.environ.get("API_URL")
+API_KEY = os.environ.get("API_KEY")
 
 
-def get_price_df(ticker, start_date, end_date):
-    print("Hello World")
+def get_price_df(ticker):
+    resp = requests.get(
+        "{}/{}".format(API_URL, ticker), params={"serietype": "line", "apikey": API_KEY}
+    )
+
+    historical_data = resp.json()["historical"]
+    historical_data_df = pd.DataFrame(historical_data)
+    historical_data_df = historical_data_df.set_index("date").sort_index()
+
+    print(historical_data_df.head())
+
+    plot = historical_data_df.plot(figsize=(20, 10))
+    plot.set_ylabel("Value of investment")
+    plot.set_title("Investment Return")
+    plot.get_legend().remove()
+    plot.figure.savefig("graphs/test.jpg")
 
     return True
 
@@ -15,4 +35,4 @@ if __name__ == "__main__":
 
     # /SPY?serietype=line&apikey=455df261819241f4292b36bf952efbe9
 
-    get_price_df(ticker, start_date, end_date)
+    get_price_df(ticker)
