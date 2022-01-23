1eafef085a1c731019aef72bb418b308d41ad500
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-23 09:56:44 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+
+
+def get_price_df(ticker, start_date, end_date):
+    print("Hello World")
+
+    return True
+
+
+if __name__ == "__main__":
+    ticker = sys.argv[1]
+    start_date = sys.argv[2]
+    end_date = sys.argv[3]
+    get_price_df(ticker, start_date, end_date)
