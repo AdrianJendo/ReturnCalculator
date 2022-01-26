c1e74ddde094af6d925ec0d1a2a8d593a6ad76a1
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-25 21:03:45 -0800

    Add logic to handle nonzero initial investment

--- main.py
+++ main.py
@@ -18,7 +18,12 @@ def get_price_df(ticker, start_date, end_date):
     )
 
     price_data = price_data.json()["historical"]
-    dividend_data = dividend_data.json()["historical"]
+    dividend_data = dividend_data.json().get(
+        "historical",
+        [
+            {"date": end_date, "adjDividend": 0}
+        ],  # Dummy data to avoid errors if company never paid a dividend
+    )
     historical_data_df = pd.DataFrame(price_data)
     historical_data_df = historical_data_df.set_index("date").sort_index()
     dividend_df = pd.DataFrame(
@@ -34,7 +39,8 @@ def plot_df(df, ticker):
     plot.set_ylabel("Value of investment")
     plot.set_title("Investment Return")
     plot.legend(loc="upper left")
-    plot.figure.savefig("graphs/{}.jpg".format(ticker))
+    date_today = datetime.today().strftime("%Y-%m-%d")
+    plot.figure.savefig("graphs/{}-{}.jpg".format(ticker, date_today))
 
 
 def get_time_delta(frequency):
@@ -57,6 +63,9 @@ if __name__ == "__main__":
     end_date = sys_args[3]
     contribution = int(sys.argv[4]) if len(sys_args) > 4 else 100
     frequency = sys.argv[5] if len(sys_args) > 5 else "monthly"
+    initial_investment = (
+        int(sys.argv[6]) if len(sys_args) > 6 else 0
+    )  # Initial investment
 
     timedelta = get_time_delta(frequency)
     cur_datetime = datetime.strptime(start_date, "%Y-%m-%d")
@@ -66,15 +75,16 @@ if __name__ == "__main__":
         "%Y-%m-%d"
     )  # avoid weekends
 
-    total_investment = 0
-    num_shares = 0  # assuming we can't do fractional
-    remainder = 0
-
-    price_df = get_price_df(ticker, start_date, end_date)
-
     if end_datetime < cur_datetime:
         print("ERROR: End date before start date")
         sys.exit(0)
+
+    price_df = get_price_df(ticker, start_date, end_date)
+
+    initial_price = price_df.iloc[0]["close"]
+    num_shares = initial_investment // initial_price  # assuming we can't do fractional
+    total_investment = initial_investment // initial_price * initial_price
+    remainder = initial_investment % initial_price
 
     return_summary_df = pd.DataFrame(
         columns=["date", "total_investment", "portfolio_value"]
