57df765f7109b5d0409211a6eda9e7b28481d88b
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-25 20:33:14 -0800

    Add dividend logic and create working script

--- main.py
+++ main.py
@@ -10,22 +10,30 @@ API_KEY = os.environ.get("API_KEY")
 
 
 def get_price_df(ticker, start_date, end_date):
-    resp = requests.get(
+    price_data = requests.get(
         "{}/{}".format(API_URL, ticker), params={"serietype": "line", "apikey": API_KEY}
     )
+    dividend_data = requests.get(
+        "{}/stock_dividend/{}".format(API_URL, ticker), params={"apikey": API_KEY}
+    )
 
-    historical_data = resp.json()["historical"]
-    historical_data_df = pd.DataFrame(historical_data)
+    price_data = price_data.json()["historical"]
+    dividend_data = dividend_data.json()["historical"]
+    historical_data_df = pd.DataFrame(price_data)
     historical_data_df = historical_data_df.set_index("date").sort_index()
+    dividend_df = pd.DataFrame(
+        [{"date": x["date"], "dividend": x["adjDividend"]} for x in dividend_data]
+    )
+    dividend_df = dividend_df.set_index("date").sort_index()
 
-    return historical_data_df.loc[start_date:end_date]
+    return historical_data_df.loc[start_date:end_date].join(dividend_df)
 
 
 def plot_df(df, ticker):
     plot = df.plot(figsize=(20, 10))
     plot.set_ylabel("Value of investment")
     plot.set_title("Investment Return")
-    plot.get_legend().remove()
+    plot.legend(loc="upper left")
     plot.figure.savefig("graphs/{}.jpg".format(ticker))
 
 
@@ -51,13 +59,61 @@ if __name__ == "__main__":
     frequency = sys.argv[5] if len(sys_args) > 5 else "monthly"
 
     timedelta = get_time_delta(frequency)
-    start_datetime = datetime.strptime(start_date, "%Y-%m-%d")
+    cur_datetime = datetime.strptime(start_date, "%Y-%m-%d")
+    last_datetime = cur_datetime
+    end_datetime = datetime.strptime(end_date, "%Y-%m-%d")
+    start_date = (cur_datetime - relativedelta(days=5)).strftime(
+        "%Y-%m-%d"
+    )  # avoid weekends
 
-    first_time = start_datetime.strftime("%Y-%m-%d")
-    next_increment = (start_datetime + timedelta).strftime("%Y-%m-%d")
+    total_investment = 0
+    num_shares = 0  # assuming we can't do fractional
+    remainder = 0
 
-    print(first_time, next_increment)
+    price_df = get_price_df(ticker, start_date, end_date)
 
-    # price_df = get_price_df(ticker, start_date, end_date)
+    if end_datetime < cur_datetime:
+        print("ERROR: End date before start date")
+        sys.exit(0)
 
-    # print(price_df)
+    return_summary_df = pd.DataFrame(
+        columns=["date", "total_investment", "portfolio_value"]
+    )
+
+    while end_datetime > cur_datetime:
+        cur_date = cur_datetime.strftime("%Y-%m-%d")
+        last_date = last_datetime.strftime("%Y-%m-%d")
+        stock_price = price_df.loc[start_date:cur_date].iloc[-1]["close"]
+        dividend = price_df.loc[last_date:cur_date]["dividend"].sum()
+
+        investible_dollars = contribution + remainder + dividend * num_shares
+        num_shares += investible_dollars // stock_price
+        total_investment += investible_dollars // stock_price * stock_price
+        remainder = investible_dollars % stock_price
+
+        return_summary_df = pd.concat(
+            [
+                return_summary_df,
+                pd.DataFrame(
+                    [
+                        [
+                            cur_date,
+                            round(total_investment, 2),
+                            round(num_shares * stock_price, 2),
+                        ]
+                    ],
+                    columns=["date", "total_investment", "portfolio_value"],
+                ),
+            ]
+        )
+
+        last_datetime = cur_datetime
+        cur_datetime += timedelta
+
+    return_summary_df = return_summary_df.set_index("date").sort_index()
+    print("Start of df: ")
+    print(return_summary_df.head(5))
+    print("End of df: ")
+    print(return_summary_df.tail(5))
+
+    plot_df(return_summary_df, ticker)
