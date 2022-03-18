790afebee3160cd58f2a823844840cf314287709
Adrian Jendo <ajendo@uwaterloo.ca>
2022-03-17 20:34:47 -0700

    Update main.py with new frequency logic and options parser

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import requests
 import os
 import pandas as pd
@@ -58,53 +57,7 @@ def get_time_delta(frequency):
         return relativedelta(months=1)
 
 
-if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option(
-        "-t",
-        "--ticker",
-        default="SPY",
-        dest="ticker",
-        help="ticker to calculate return",
-    )
-    parser.add_option(
-        "-s",
-        "--start_date",
-        default=(datetime.now() - relativedelta(years=1)).strftime("%Y-%m-%d"),
-        dest="start_date",
-        help="date to start calculate",
-    )
-    parser.add_option(
-        "-e",
-        "--end_date",
-        default=(datetime.now()).strftime("%Y-%m-%d"),
-        dest="end_date",
-        help="date to end calculate",
-    )
-    parser.add_option(
-        "-c",
-        "--contribution",
-        default=100,
-        dest="contribution",
-        help="recurring contribution",
-    )
-    parser.add_option(
-        "-f",
-        "--frequency",
-        default="monthly",
-        dest="frequency",
-        help="frequency of recurring contribution",
-    )
-    parser.add_option(
-        "-i",
-        "--initial_investment",
-        default=0,
-        dest="initial_investment",
-        help="initial contribution",
-    )
-
-    (options, args) = parser.parse_args()
-
+def main(options):
     ticker = options.ticker
     start_date = options.start_date
     end_date = options.end_date
@@ -112,9 +65,13 @@ if __name__ == "__main__":
     frequency = options.frequency
     initial_investment = options.initial_investment
 
-    timedelta = get_time_delta(frequency)
     cur_datetime = datetime.strptime(start_date, "%Y-%m-%d")
-    last_datetime = cur_datetime
+    next_contribution = cur_datetime + get_time_delta(
+        frequency
+    )  # Next contribution to portfolio
+    last_contribution = cur_datetime - relativedelta(
+        days=7
+    )  # prevents error when last_date and start_date are same day
     end_datetime = datetime.strptime(end_date, "%Y-%m-%d")
     start_date = (cur_datetime - relativedelta(days=5)).strftime(
         "%Y-%m-%d"
@@ -122,7 +79,7 @@ if __name__ == "__main__":
 
     if end_datetime < cur_datetime:
         print("ERROR: End date before start date")
-        sys.exit(0)
+        return
 
     price_df = get_price_df(ticker, start_date, end_date)
 
@@ -137,15 +94,20 @@ if __name__ == "__main__":
 
     while end_datetime > cur_datetime:
         cur_date = cur_datetime.strftime("%Y-%m-%d")
-        last_date = last_datetime.strftime("%Y-%m-%d")
-        stock_price = price_df.loc[start_date:cur_date].iloc[-1]["close"]
-        dividend = price_df.loc[last_date:cur_date]["dividend"].sum()
+        last_date = last_contribution.strftime("%Y-%m-%d")
+        stock_price = price_df.loc[last_date:cur_date].iloc[-1]["close"]
 
-        investible_dollars = contribution + remainder + dividend * num_shares
-        num_shares += investible_dollars // stock_price
-        total_investment += investible_dollars // stock_price * stock_price
-        remainder = investible_dollars % stock_price
+        # Only update total investment at each contribution
+        if next_contribution <= cur_datetime:
+            next_contribution += get_time_delta(frequency)
+            dividend = price_df.loc[last_date:cur_date]["dividend"].sum()
+            investible_dollars = contribution + remainder + dividend * num_shares
+            num_shares += investible_dollars // stock_price
+            total_investment += investible_dollars // stock_price * stock_price
+            remainder = investible_dollars % stock_price
+            last_contribution = cur_datetime
 
+        # Update graph every 7 days
         return_summary_df = pd.concat(
             [
                 return_summary_df,
@@ -162,8 +124,7 @@ if __name__ == "__main__":
             ]
         )
 
-        last_datetime = cur_datetime
-        cur_datetime += timedelta
+        cur_datetime += relativedelta(days=7)
 
     return_summary_df = return_summary_df.set_index("date").sort_index()
     print("Start of df: ")
@@ -172,3 +133,55 @@ if __name__ == "__main__":
     print(return_summary_df.tail(5))
 
     plot_df(return_summary_df, ticker)
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option(
+        "-t",
+        "--ticker",
+        default="SPY",
+        dest="ticker",
+        help="ticker to calculate return",
+    )
+    parser.add_option(
+        "-s",
+        "--start_date",
+        default=(datetime.now() - relativedelta(years=10)).strftime("%Y-%m-%d"),
+        dest="start_date",
+        help="date to start calculate",
+    )
+    parser.add_option(
+        "-e",
+        "--end_date",
+        default=(datetime.now()).strftime("%Y-%m-%d"),
+        dest="end_date",
+        help="date to end calculate",
+    )
+    parser.add_option(
+        "-c",
+        "--contribution",
+        default=100,
+        dest="contribution",
+        help="recurring contribution",
+        type="int",
+    )
+    parser.add_option(
+        "-f",
+        "--frequency",
+        default="monthly",
+        dest="frequency",
+        help="frequency of recurring contribution",
+    )
+    parser.add_option(
+        "-i",
+        "--initial_investment",
+        default=0,
+        dest="initial_investment",
+        help="initial contribution",
+        type="int",
+    )
+
+    (options, args) = parser.parse_args()
+
+    main(options)
